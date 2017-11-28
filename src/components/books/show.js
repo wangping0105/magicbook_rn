63eb9fc3c701f3edffd4ed34d9efe1d329de9c75
jack ping <525399584@qq.com>
2017-11-28 23:32:07 +0800

    add pull view

--- src/components/books/show.js
+++ src/components/books/show.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react'
 import {TouchableOpacity, StyleSheet, View, Text, ListView} from 'react-native'
+import {PullList} from 'react-native-pullview'
+import Utils from '../../helpers/utils'
 
 class BookShow extends Component {
     static navigationOptions = ({ navigation }) => {
@@ -22,25 +24,15 @@ class BookShow extends Component {
             per_page: 10,
             total_page: 1
         };
+        this.renderHeader = this.renderHeader.bind(this);
+        this.renderRow = this.renderRow.bind(this);
+        // this.renderFooter = this.renderFooter.bind(this);
+        this.loadMore = this.loadMore.bind(this);
+        this.topIndicatorRender = this.topIndicatorRender.bind(this);
     };
 
     componentDidMount() {
-        query = {
-            page: this.state.page,
-            per_page: this.state.per_page
-        }
-
-        fetch("http://47.91.157.26/api/v1/books/"+ this.state.id + "?" + query)
-        .then((response) => response.json())
-        .then((responseJson) => {
-            this.setState({
-                book: responseJson.data.book,
-                chapters : this.state.chapters.cloneWithRows(responseJson.data.book_chapters)
-            })
-        })
-        .catch((error) => {
-            console.error(error);
-        });
+        this.fetch_chapter({})
     };
 
     _onPressButton(id, title){
@@ -51,7 +43,74 @@ class BookShow extends Component {
         }})
     }
 
-    _renderRow(rowData){
+
+
+    render () {
+        return (
+            <View>
+                <PullList
+                    style={{}}
+                    onPullRelease={this.onPullRelease}
+                    topIndicatorRender={this.topIndicatorRender}
+                    topIndicatorHeight={60}
+                    renderHeader={this.renderHeader}
+                    pageSize={5}
+                    initialListSize={5}
+                    onEndReached={this.loadMore}
+                    onEndReachedThreshold={60}
+                    // renderFooter={this.renderFooter}
+                    dataSource={this.state.chapters}
+                    renderRow={this.renderRow}
+                    enableEmptySections={true}/>
+
+            </View>
+        )
+    }
+
+    onPullRelease(resolve) {
+        //do something
+        setTimeout(() => {
+            resolve();
+        }, 3000);
+    }
+
+    topIndicatorRender(pulling, pullok, pullrelease) {
+        const hide = {position: 'absolute', left: -10000};
+        const show = {position: 'relative', left: 0};
+        setTimeout(() => {
+            if (pulling) {
+                this.txtPulling && this.txtPulling.setNativeProps({style: show});
+                this.txtPullok && this.txtPullok.setNativeProps({style: hide});
+                this.txtPullrelease && this.txtPullrelease.setNativeProps({style: hide});
+            } else if (pullok) {
+                this.txtPulling && this.txtPulling.setNativeProps({style: hide});
+                this.txtPullok && this.txtPullok.setNativeProps({style: show});
+                this.txtPullrelease && this.txtPullrelease.setNativeProps({style: hide});
+            } else if (pullrelease) {
+                this.txtPulling && this.txtPulling.setNativeProps({style: hide});
+                this.txtPullok && this.txtPullok.setNativeProps({style: hide});
+                this.txtPullrelease && this.txtPullrelease.setNativeProps({style: show});
+            }
+        }, 1);
+        return (
+            <View style={{flexDirection: 'row', justifyContent: 'center', alignItems: 'center', height: 60}}>
+                <ActivityIndicator size="small" color="gray" />
+                <Text ref={(c) => {this.txtPulling = c;}}>当前PullList状态: pulling...</Text>
+                <Text ref={(c) => {this.txtPullok = c;}}>当前PullList状态: pullok......</Text>
+                <Text ref={(c) => {this.txtPullrelease = c;}}>当前PullList状态: pullrelease......</Text>
+            </View>
+        );
+    }
+
+    renderHeader() {
+        return (
+            <View style={{height: 50, backgroundColor: '#eeeeee', alignItems: 'center', justifyContent: 'center'}}>
+                <Text style={{fontWeight: 'bold'}}>This is header</Text>
+            </View>
+        );
+    }
+
+    renderRow(rowData){
         return (
             <TouchableOpacity
                 activeOpacity={0.5}
@@ -66,17 +125,49 @@ class BookShow extends Component {
         );
     }
 
-    render () {
-        return (
-            <View>
-                <ListView
-                    dataSource={this.state.chapters}
-                    renderRow={this._renderRow.bind(this)}
-                    enableEmptySections={true}/>
+    // renderFooter() {
+    //     if(this.state.nomore) {
+    //         return null;
+    //     }
+    //     return (
+    //         <View style={{height: 100}}>
+    //             <ActivityIndicator />
+    //         </View>
+    //     );
+    // }
 
-            </View>
-        )
+    loadMore() {
+        var page = this.state.page;
+        var total_page = this.state.total_page;
+        if(page == total_page){
+            page = page + 1;
+            var query = {
+                page: page,
+                per_page: this.state.per_page
+            }
+            this.fetch_chapter(query)
+        }
     }
+
+    fetch_chapter(query){
+        Utils.parse_params(query);
+        console.log("http://47.91.157.26/api/v1/books/"+ this.state.id + Utils.parse_params(query))
+        fetch("http://47.91.157.26/api/v1/books/"+ this.state.id + Utils.parse_params(query))
+            .then((response) => response.json())
+            .then((responseJson) => {
+                var total_page = (responseJson.data.total_count / responseJson.data.perpage) + 1;
+                this.setState({
+                    book: responseJson.data.book,
+                    chapters : this.state.chapters.cloneWithRows(responseJson.data.book_chapters),
+                    total_page: total_page,
+                    page: responseJson.data.page
+                })
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
+
 }
 
 const styles =StyleSheet.create({
---
+++ src/helpers/utils.js
@@ -0,0 +1,13 @@
+
+export default Utils = {
+    parse_params: function(params){
+        let str = '?';
+        for(let key in params){
+            if(params[key] === undefined || params[key] === null || params[key] === ''){
+                continue;
+            }
+            str += key + '=' + params[key] + '&';
+        }
+        return str + 's=0';
+    }
+}
